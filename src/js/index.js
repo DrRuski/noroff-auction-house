7a76b809fae468932faca2eed1697171c190dd31
DrRuski <ilia_e@hotmail.com>
2023-05-15 12:40:55 +0200

    updated default js index barrel links

--- src/js/index.js
+++ src/js/index.js
@@ -2,55 +2,55 @@ import * as utilities from "./utilities/storage/index.mjs";
 import * as management from "./listeners/index.mjs";
 import * as templates from "./templates/index.js";
 import * as render from "./setupRender/index.mjs";
+const siteFolder = "/noroff-auction-house";
 
 switch (window.location.pathname) {
-  case "/login/index.html":
-  case "/login/":
-  case "/html/login":
+  case `${siteFolder}/login/index.html`:
+  case `${siteFolder}/login/`:
+  case `${siteFolder}/login`:
     management.loginListener();
     break;
-  case "/registration/index.html":
-  case "/registration/":
-  case "/registration":
+  case `${siteFolder}/registration/index.html`:
+  case `${siteFolder}/registration/`:
+  case `${siteFolder}/registration`:
     management.regisListener();
     break;
-  case "/home/index.html":
-  case "/home/":
-  case "/home":
+  case `${siteFolder}/home/index.html`:
+  case `${siteFolder}/home/`:
+  case `${siteFolder}/home`:
     render.renderHome();
     break;
-  case "/collection/index.html":
-  case "/collection/":
-  case "/collection":
+  case `${siteFolder}/collection/index.html`:
+  case `${siteFolder}/collection/`:
+  case `${siteFolder}/collection`:
     render.renderOwnedCollection();
     render.renderYourBidsCollection();
     render.renderListedItemsCollection();
     break;
-  case "/profile/index.html":
-  case "/profile/":
-  case "/profile":
+  case `${siteFolder}/profile/index.html`:
+  case `${siteFolder}/profile/`:
+  case `${siteFolder}/profile`:
     templates.renderUserProfile();
     break;
-  case "/item/sell/index.html":
-  case "/item/sell/":
-  case "/item/sell":
+  case `${siteFolder}/item/sell/index.html`:
+  case `${siteFolder}/item/sell/`:
+  case `${siteFolder}/item/sell`:
     management.createListingItem();
     break;
-  case "/profile/edit/index.html":
-  case "/profile/edit/":
-  case "/profile/edit":
+  case `${siteFolder}/profile/edit/index.html`:
+  case `${siteFolder}/profile/edit/`:
+  case `${siteFolder}/profile/edit`:
     management.updateProfile();
     break;
-  case "/item/view/index.html":
-  case "/item/view/":
-  case "/item/view":
+  case `${siteFolder}/item/view/index.html`:
+  case `${siteFolder}/item/view/`:
+  case `${siteFolder}/item/view`:
     render.renderSingleItem();
-
     break;
 }
 if (
-  window.location.pathname !== "/noroff-auction-house/login/index.html" &&
-  window.location.pathname !== "/noroff-auction-house/registration/index.html"
+  window.location.pathname !== `${siteFolder}/login/index.html` &&
+  window.location.pathname !== `${siteFolder}/registration/index.html`
 ) {
   utilities.navbarResizing();
   management.userCurrencyDisplay();
